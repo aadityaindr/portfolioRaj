bf785a4ab8056c0d7b64186b3ada9b9d1307d6de
aadi-69 <137440229+aadi-69@users.noreply.github.com>
2024-11-08 00:34:13 +0530

    all files added

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const port = 3019
+
+const app = express();
+app.use(express.static(__dirname))
+app.use(express.urlencoded({extended:true}))
+
+
+mongoose.connect("mongodb+srv://aaditya:aaditya@cluster0.jgjbq.mongodb.net/")
+const db = mongoose.connection
+db.once('open', ()=>{
+    console.log("mongodb connected")
+})
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+})
+
+const Users = mongoose.model("data", userSchema)
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.post('/post', async (req, res) => {
+    try {
+        const { fullname, email, message } = req.body;
+        const user = new Users({
+            fullname,
+            email,
+            message
+        });
+
+        await user.save(); // Save the user to the database
+        // console.log(user); // Log the user data in the server console
+
+        // Send a success response to the client
+        res.status(200).json({ message: "Form submitted successfully" });
+    } catch (error) {
+        console.error(error); // Log any errors to the server console
+        res.status(500).json({ message: "Error submitting form" }); // Send an error response
+    }
+});
+
+app.listen(port,()=>{
+    console.log("Server started")
+})
+
+
